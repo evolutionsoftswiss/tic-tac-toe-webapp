bda8cbb4c56ab5229d4fdb37504d90b1cea82e25
evolutionsoft <evolutionsoft@evolutionsoft-System>
2022-08-16 06:05:22 +0200

    Use default locale from request (Browser) language instead of english

--- src/main/java/ch/evolutionsoft/game/tictactoe/view/LocaleBean.java
+++ src/main/java/ch/evolutionsoft/game/tictactoe/view/LocaleBean.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 import java.util.Locale;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named("localeBean")
 @SessionScoped
 public class LocaleBean implements Serializable {
 
-    private Locale locale = Locale.ENGLISH;
+    private Locale locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
 
     public Locale getLocale() {
         return locale;
